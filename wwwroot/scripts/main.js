7420ebfcca7b833f2392a904cccbe050c4bbf835
Pilar Barbero Iriarte <pilar.barbero@col.vueling.com>
2019-10-28 19:25:19 +0100

    finish demo

--- wwwroot/scripts/main.js
+++ wwwroot/scripts/main.js
@@ -16,3 +16,31 @@ solid.auth.trackSession(session => {
       $('#profile').val(session.webId);
   }
 });
+
+const FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/');
+$('#view').click(async function loadProfile() {
+  // Set up a local data store and associated data fetcher
+  const store = $rdf.graph();
+  const fetcher = new $rdf.Fetcher(store);
+
+  // Load the person's data into the store
+  const person = $('#profile').val();
+  await fetcher.load(person);
+
+  // Display their details
+  const fullName = store.any($rdf.sym(person), FOAF('name'));
+  $('#fullName').text(fullName && fullName.value);
+
+  // Display their friends
+  const friends = store.each($rdf.sym(person), FOAF('knows'));
+  $('#friends').empty();
+  friends.forEach(async (friend) => {
+    await fetcher.load(friend);
+    const fullName = store.any(friend, FOAF('name'));
+    $('#friends').append(
+      $('<li>').append(
+        $('<a>').text(fullName && fullName.value || friend.value)
+                .click(() => $('#profile').val(friend.value))
+                .click(loadProfile)));
+  });
+});
