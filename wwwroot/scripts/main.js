53c659b1ac173318fc21c0b9c9f8f121bc100f11
Pilar Barbero Iriarte <pilar.barbero@col.vueling.com>
2019-10-30 20:45:40 +0100

    Add friends pictures feature

--- wwwroot/scripts/main.js
+++ wwwroot/scripts/main.js
@@ -16,13 +16,17 @@ solid.auth.trackSession(session => {
       $('#profile').val(session.webId);
     }
     $("#solid-details").show();
+    $("#cardLogin").hide();
   }
   else {
     $("#solid-details").hide();
+    $("#cardLogin").show();
   }
 });
 
 const FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/');
+const VCARD = new $rdf.Namespace('http://www.w3.org/2006/vcard/ns#');
+
 $('#view').click(async function loadProfile() {
   // Set up a local data store and associated data fetcher
   const store = $rdf.graph();
@@ -31,27 +35,57 @@ $('#view').click(async function loadProfile() {
   // Load the person's data into the store
   const person = $('#profile').val();
   await fetcher.load(person);
+  let me = $rdf.sym(person);
 
-  // Display their details
-  const fullName = store.any($rdf.sym(person), FOAF('name'));
+  showUserDetails(store, me);
+  showUserFriends(store, me, fetcher);
+});
+
+
+function showUserDetails(store, me){
+  const fullName = store.any(me, FOAF('name'));
   $('#fullName').text(fullName && fullName.value);
-  $("#user-card").removeAttr("hidden");
-  $("#user-card").fadeIn(300);
+  $("#userDetails").fadeIn(300);
 
   //Display the img
-  // let picture = store.any(me, VCARD('hasPhoto')) || store.any(me, FOAF(image));
-  // console.log(picture);
+  let picture = getImage(store, me);
+  $("#user-img").attr("src", picture.value);  
+}
 
-  // Display their friends
-  const friends = store.each($rdf.sym(person), FOAF('knows'));
+function getImage(store, me){  
+  return store.any(me, VCARD('hasPhoto')) || store.any(me, FOAF('image'));
+}
+
+function showUserFriends(store, me, fetcher){
+  const friends = store.each(me, FOAF('knows'));
   $('#friends').empty();
-  friends.forEach(async (friend) => {
-    await fetcher.load(friend);
+
+  friends.forEach(async (urlName) => {
+    var urlFriend = urlName.value + "#me";
+    await fetcher.load(urlFriend);
+    let friend = $rdf.sym(urlFriend);
+    let image = getImage(store, friend);
+
     const fullName = store.any(friend, FOAF('name'));
-    $('#friends').append(
-      $('<li>').append(
-        $('<a>').text(fullName && fullName.value || friend.value)
-                .click(() => $('#profile').val(friend.value))
-                .click(loadProfile)));
+    $('#friends').append(addFriend(fullName, image, urlFriend));
   });
-});
+}
+
+function addFriend(fullName, image, urlFriend){
+  if(image === undefined){
+    image = "https://picsum.photos/200/200";
+  }  
+  else{
+    image = image.value;
+  } 
+
+  return $('<dt>').append(
+    "<div id='user-card' class='card' style='width: 18rem;' >" + 
+    "<img src='" + image + "' class='card-img-top'>" + 
+    "<div class='card-body'>" + 
+    "<h5 class='card-title'>" +fullName +"</h5>" + 
+      "  <a href='" + urlFriend +"' class='card-text'>"+ urlFriend+ "</a>" + 
+    " </div>" + 
+    "</div>"
+    );
+}
